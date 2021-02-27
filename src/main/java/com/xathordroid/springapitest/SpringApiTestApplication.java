5552ea3f872b25bb6161d3c8f25037729e257a8e
Xathor <andymarcel.97@gmail.com>
2021-02-27 22:25:14 +0100

    Initial commit.

---
+++ src/main/java/com/xathordroid/springapitest/SpringApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.xathordroid.springapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xathordroid/springapitest/SpringApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xathordroid.springapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
