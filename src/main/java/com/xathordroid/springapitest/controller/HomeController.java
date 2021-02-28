5d26b6ef96bd9fc1f5974e329dc8bda2f1d8f25a
Xathor <andymarcel.97@gmail.com>
2021-02-28 20:57:36 +0100

    Create new REST Mapping and test process to test Ascii docs.

---
+++ src/main/java/com/xathordroid/springapitest/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.xathordroid.springapitest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/home")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello, World");
+    }
+}
