d30db00d7b291be9385f5cdf1bd048d491e590cc
Xathor <andymarcel.97@gmail.com>
2021-02-28 18:29:38 +0100

    Create new GET request mapping to test request with parameters.

--- src/main/java/com/xathordroid/springapitest/controller/HelloController.java
+++ src/main/java/com/xathordroid/springapitest/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.xathordroid.springapitest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
